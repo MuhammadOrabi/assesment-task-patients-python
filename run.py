635b4cbcbb678392479d2834afae7c32b4d630ba
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2018-10-03 20:40:55 +0200

    updates for heroku

--- run.py
+++ run.py
@@ -4,12 +4,9 @@ import os
 from src.app import create_app
 
 
-def main():
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
-    # run app
-    app.run(host='0.0.0.0')
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0')
     
